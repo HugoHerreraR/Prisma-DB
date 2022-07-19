e6ca3bf6b1c6a9d180d9ea0349446d015565e486
HugoHerreraH <launchx04415@innovaccion.mx>
2022-07-18 21:23:51 -0500

    Datos cargados a la tabla ExplorerInfo

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,59 +1,101 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
+// (async function main() {
+//   try {
+//     // const user = await prisma.explorer.upsert({
+//     //   where: { name: "user" },
+//     //   update: {},
+//     //   create: {
+//     //     name: "Pepito",
+//     //     username: "ajolonauta",
+//     //     mission: "Node",
+//     //   },
+//     // });
+
+//     // const user2 = await prisma.explorer.upsert({
+//     //   where: { name: "user2" },
+//     //   update: {},
+//     //   create: {
+//     //     name: "Roberto",
+//     //     username: "robert",
+//     //     mission: "Node",
+//     //   },
+//     // });
+
+//     // const user3 = await prisma.explorer.upsert({
+//     //   where: { name: "user3" },
+//     //   update: {},
+//     //   create: {
+//     //     name: "Renata",
+//     //     username: "nati",
+//     //     mission: "Java",
+//     //   },
+//     // });
+
+//     // const user4 = await prisma.explorer.upsert({
+//     //   where: { name: "user4" },
+//     //   update: {},
+//     //   create: {
+//     //     name: "Woopa 3",
+//     //     username: "ajolonauta3",
+//     //     mission: "Node",
+//     //   },
+//     // });
+
+//     // const user5 = await prisma.explorer.upsert({
+//     //   where: { name: "user5" },
+//     //   update: {},
+//     //   create: {
+//     //     name: "Paola",
+//     //     username: "pao",
+//     //     mission: "Node",
+//     //   },
+//     // });
+
+//     const user6 = await prisma.explorer.upsert({
+//       where: { name: "user6" },
+//       update: {},
+//       create: {
+//         name: "Maria",
+//         username: "mari",
+//         mission: "Node",
+//       },
+//     });
+
+//     console.log("Create 5 explorers");
+//   } catch (e) {
+//     console.error(e);
+//     process.exit(1);
+//   } finally {
+//     await prisma.$disconnect();
+//   }
+// })();
+
 (async function main() {
   try {
-    const user = await prisma.explorer.upsert({
-      where: { name: "user" },
+    const explorer = await prisma.ExplorerInfo.upsert({
+      where: {name: "explorer"},
       update: {},
       create: {
-        name: "Pepito",
-        username: "ajolonauta",
-        mission: "Node",
-      },
+        name: "Sara",
+        lang: "Desconocido",
+        missionCommander: "Carlos",
+        enrollments: 3
+      }
     });
-
-    const user2 = await prisma.explorer.upsert({
-      where: { name: "user2" },
+    const explorer2 = await prisma.ExplorerInfo.upsert({
+      where: {name: "explorer2"},
       update: {},
       create: {
-        name: "Roberto",
-        username: "robert",
-        mission: "Node",
-      },
+        name: "Laura",
+        lang: "Desconocido",
+        missionCommander: "Juan",
+        enrollments: 1
+      }
     });
 
-    const user3 = await prisma.explorer.upsert({
-      where: { name: "user3" },
-      update: {},
-      create: {
-        name: "Renata",
-        username: "nati",
-        mission: "Java",
-      },
-    });
-
-    const user4 = await prisma.explorer.upsert({
-      where: { name: "user4" },
-      update: {},
-      create: {
-        name: "Woopa 3",
-        username: "ajolonauta3",
-        mission: "Node",
-      },
-    });
-
-    const user5 = await prisma.explorer.upsert({
-      where: { name: "user5" },
-      update: {},
-      create: {
-        name: "Paola",
-        username: "pao",
-        mission: "Node",
-      },
-    });
-
-    console.log("Create 5 explorers");
+    console.log("Create 2 explorersinfo");
   } catch (e) {
     console.error(e);
     process.exit(1);
